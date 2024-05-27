4665856e9981e67b8f351fee13667af17085bc57
Samuel Odongo <samieodd@gmail.com>
2024-05-27 12:23:19 +0300

    Split work with multiprocessing

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+import concurrent.futures.process
+import csv
+
 from scraper import Scraper
 
 
-def main() -> None:
+def main(start: int, end: int) -> None:
     scraper: Scraper = Scraper()
-    scraper.get_data()
+    scraper.get_data(start=start, end=end + 1)
+    with open(f'charities{start}_{end}.csv', 'w', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(['EIN', 'Name', 'Phone', 'Website', 'Address', 'Profile Link'])
+        for charity in scraper.results:
+            writer.writerow([charity.ein, charity.name, charity.phone, charity.website, charity.address, charity.profileLink])
+        f.close()
 
 
 if __name__ == '__main__':
-    main()
+    ranges = [(0, 100), (100, 200), (200, 300), (300, 400), (400, 500), (500, 600), (600, 700), (700, 800), (800, 900), (900, 1000)]
+    with concurrent.futures.process.ProcessPoolExecutor(max_workers=len(ranges)) as executor:
+        results = list(executor.map(main, *zip(*ranges)))
+
